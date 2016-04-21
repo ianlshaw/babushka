600f8d6dc58a94510c64ff6b37fa67e6888dcfe3
Ian Shaw <ian.shaw@thirtythree.co.uk>
2016-04-21 14:09:13 +0100

    removed superfluous } from corosync_config_exists.rb

--- corosync_config_exists.rb
+++ corosync_config_exists.rb
@@ -2,7 +2,6 @@ dep 'corosync config exists' do
   met? {
     "test -e /etc/corosync/corosync.conf"
     }
-  }
   meet {
     # dont know how to interpolate files yet.
     log_shell 'Making config file.', 'touch /etc/corosync/corosync.conf'
