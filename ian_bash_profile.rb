68624999e1d9c8f13e9d65cd9d838ac82b2d32ee
Ian Shaw <ian.shaw@thirtythree.co.uk>
2017-03-01 13:01:14 +0000

    removed spaces from dep name

--- ian_bash_profile.rb
+++
@@ -1,9 +0,0 @@
-dep 'ian bash colors' do
-  met? { 
-    '/home/ian/.bash_profile'.p.grep(/^PS/) 
-  }
-
-  meet { 
-    '/home/.bash_profile'.p.append('PS1="\[\033[1;31m\]\u@\h:\w$\[\033[0m\ "')
-  }
-end
