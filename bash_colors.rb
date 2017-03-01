495ae1508e0dd3492b782305202124da93af3868
Ian Shaw <ian.shaw@thirtythree.co.uk>
2017-03-01 13:39:05 +0000

    updated bash_colors.rb with overkill

--- bash_colors.rb
+++ bash_colors.rb
@@ -4,6 +4,6 @@ dep 'bash_colors' do
   }
 
   meet { 
-    '~/.bash_profile'.p.append('PS1="\[\033[1;31m\]\u@\h:\w$\[\033[0m\ "')
+    '~/.bash_profile'.p.append('PS1="\[\e[1;36m\]\d \[\e[1;32m\]\t \[\e[1;33m\]\u@\[\e[1;35m\]\h:\w\$\[\e[0;39m\] "')
   }
 end
